83c753c606f10f2a0f9bec759f78633192eb0717
G.Cai <ic.ve.cv@gmail.com>
2017-02-12 20:28:06 +1100

    merged ard lcd sketch with main, added temporary testing for host-side lib (lcdControl.py)

--- hostscripts/lib/lcdControl.py
+++ hostscripts/lib/lcdControl.py
@@ -4,9 +4,10 @@ from time import sleep
 
 class Lcd:
 
-    def __init__(self, dev, cmdmap="", size=(16, 2), bufsize=40, curpos=0, scpos=0):
+    def __init__(self, dev, cmdmap="", size=(16, 2), bufsize=40, curpos=0, scpos=0, lcdParsecmd='x'):
         self.dev = dev
         self.size = size
+        self.lcdParsecmd = lcdParsecmd
         # create buffer
         self.buf = [[" " for c in range(size[0])] for r in range(size[1])]
         self.bufsize = bufsize
@@ -42,9 +43,10 @@ class Lcd:
     def rpush(self, rcmd):
         '''Does not advise user to use since this skips the cursor update'''
         if type(rcmd) is int:
-            cmd = chr(rcmd) + ";"
+            cmd = self.lcdParsecmd + chr(rcmd) + ";"
         else:
-            cmd = "".join(map(chr, rcmd)) + ";"
+            # for setCursor
+            cmd = self.lcdParsecmd + "".join(map(chr, rcmd)) + ";"
         self.dev.write(cmd.encode())
 
     def push(self, ky):
@@ -59,84 +61,56 @@ class Lcd:
             self.updcur(1)
             return
 
-
     def setCursor(self, c, r):
-        self.rpush([self.cmdmap["setCursor"], c, r])
+        # self.rpush([self.cmdmap["setCursor"], c, r])
+        # displace to avoid transmitting 0, as weirdness occurs on rpi-ard when 0 is send?
+        # please alos see the ard sketch for dedisplacement
+        self.dev.write(("x" + chr(32) + chr(c + 1) + chr(r + 1) + ";").encode())
         self.updcur([c, r], True)
 
     def print(self, txt):
-        self.dev.write((chr(self.cmdmap["print"]) + txt + ";").encode())
+        self.dev.write((self.lcdParsecmd + chr(self.cmdmap["print"]) + txt + ";").encode())
         self.updcur(len(txt))
-    
+
     def updcur(self, val, raw=False):
         'flag raw to set instead of displace curpos'
         if type(val) is list:
             val = self.convpos(val)
         if raw:
             self.curpos = val
-            print(val)
             return
         else:
             rval = val + self.curpos
             totalbuf = self.bufsize * self.size[1]
             # modding works for both pos and neg val
             self.curpos = rval % totalbuf
-        print(self.curpos)
-            
+
     def convpos(self, curpos):
         if type(curpos) is list:
-            return curpos[0]  + curpos[1] * self.bufsize
+            return curpos[0] + curpos[1] * self.bufsize
         elif type(curpos) is int:
             return [curpos % self.bufsize, int(curpos / self.bufsize)]
-    
-            
-usb = Serial("COM5")
-lcd = Lcd(usb)
 
-sleep(3)
-# rcmd = [32, 0, 1]
+# usb = Serial("/dev/ttyUSB0")
+# lcd = Lcd(usb)
 
-# lcd.rpush([32, 0, 1])
-
-# usb.write((chr(33) + "World;").encode())
-
-# lcd.rpush(38)
-# lcd.rpush(36)
-# lcd.rpush(41)
-
-# # usb.write((chr(33) + "123456789abcdefgh;").encode())
-# # lcd.rpush(50)
-
-# lcd.rpush([32, 10, 0])
-
-# lcd.rpush(53)
-# usb.write((chr(33) + "abc;").encode())
-# lcd.rpush(51)
-# lcd.rpush(51)
-# lcd.setCursor(15, 1)
-lcd.push("home")
-# lcd.push("autoscroll")
-lcd.push("cursor")
-# lcd.setCursor(0, 39)
-p = "ASCII stands for American Standard Code for Information Interchange."
-p = "3.141592653589793238462643383279502884197169399375105820974944592307816406286"
-p = "3.141592653589793238462643383279502884193.14159265358979323846264338327950288419"
+# sleep(3)
+# lcd.push("sensor")
+# p = "3.141592653589793238462643383279502884197169399375105820974944592307816406286"
+# p = "3.141592653589793238462643383279502884193.14159265358979323846264338327950288419"
 # p = "3.14159265358979323846264338327950288419"
-lcd.print(p)
-lcd.setCursor(5, 1)
-print(lcd.curpos)
-# for i, c in enumerate(p):
-#     sleep(.25)
-#     lcd.print(c)
-        # if i > 14:
-        #     lcd.push("scrollDisplayLeft")
-    # lcd.push("home")
-# while True:
-#     sleep(.2)
-#     lcd.push("scrollDisplayRight")
-# sleep(5)
+# lcd.print(p)
+# # lcd.setCursor(5, 1)
+# # print(lcd.curpos)
 
-#lcd.push("clear")
-#lcd.setCursor(0, 41)
-#lcd.print("x")
-# sleep(1) 
+# import time
+# lcd.setCursor(0, 1)
+# sleep(1)
+# lcd.push("clear")
+# while True:
+#     sleep(.5)
+#     lcd.setCursor(0, 0)
+#     lcd.print(time.strftime("%x"))
+#     lcd.setCursor(0, 1)
+#     lcd.print(time.strftime("%X"))
+
--- src/current_sketch.ino
+++ src/current_sketch.ino
@@ -1,24 +1,143 @@
 #include "LedControl.h"
+#include <Wire.h>  // Comes with Arduino IDE
+#include <LiquidCrystal_I2C.h>
 
+  // pin to sink LED current from 3.3 v source
 #define motionSensor 5
 #define motionLED 6
 #define numOfDisplay 2
 LedControl lc=LedControl(2,3,4,numOfDisplay);
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+//7svg serup
+void ssvgSetup(){
+  lc.shutdown(0, false);
+  lc.setIntensity(0, 0);
+  lc.clearDisplay(0);
+  lc.setDigit(0, 2, 10, true);
+
+}
+
+void motSetup(){
+  pinMode(motionLED, OUTPUT);
+  digitalWrite(motionLED, HIGH);
+  pinMode(motionSensor, INPUT);
+}
+
+void lcdSetup(){
+  lcd.begin(16,2);
+  lcd.print("HELLO");
 
 
+}
 void setup() {
-  // put your setup code here, to run once:
-    // pin to sink LED current from 3.3 v source
-    pinMode(motionLED, OUTPUT);
-    digitalWrite(motionLED, HIGH);
-    pinMode(motionSensor, INPUT);
-    lc.shutdown(0, false);
-    lc.setIntensity(0, 0);
-    lc.clearDisplay(0);
-    lc.setDigit(0, 2, 10, true);
-    Serial.begin(9600);
+  //motionSensor
+  motSetup();
+  //7 sevgment displays setup
+  ssvgSetup();
+  //lcd setup
+  lcdSetup();
+
+  Serial.begin(9600);
+
     
 }
+
+void lcdParse(String cmd){
+  
+  byte func = cmd.charAt(0);
+  switch (func){
+    
+    case 32:
+      lcd.setCursor(cmd.charAt(1) - 1, cmd.charAt(2) - 1);
+    break;
+
+    case 33:
+      lcd.print(cmd.substring(1));
+    break;
+    
+    case 34:
+      lcd.backlight();
+    break;
+
+    case 35:
+      lcd.noBacklight();
+    break;
+
+    case 36:
+      lcd.blink();
+    break;
+    
+    case 37:
+      lcd.noBlink();
+    break;
+
+    case 38:
+      lcd.cursor();
+    break;
+
+    case 39:
+      lcd.noCursor();
+    break;
+
+    case 40:
+      lcd.clear();
+    break;
+
+    case 41:
+      lcd.home();
+    break;
+
+    case 42:
+      lcd.moveCursorLeft();
+    break;
+
+    case 43:
+      lcd.moveCursorRight();
+    break;
+
+    case 44:
+      lcd.autoscroll();
+    break;
+
+    case 45:
+      lcd.noAutoscroll();
+    break;
+
+    case 46:
+      lcd.on();
+    break;
+
+    case 47:
+      lcd.off();
+    break;
+
+    case 48:
+      lcd.display();
+    break;
+
+    case 49:
+      lcd.noDisplay();
+    break;
+
+    case 50:
+      lcd.scrollDisplayLeft();
+    break;
+
+    case 51:
+      lcd.scrollDisplayRight();
+    break;
+
+    case 52:
+      lcd.leftToRight();
+    
+    case 53:
+      lcd.rightToLeft();
+    break;
+
+
+  }}
+
 
 int char2i(char x){
    return x - '0';
@@ -27,6 +146,10 @@ int char2i(char x){
 void parseRun(String cmd){
     char func = cmd.charAt(0);
     switch (func){
+      // i2c 16x2 charactors lcd api
+      case 'x':
+        lcdParse(cmd.substring(1));
+      break;
       //---------------
       // motionLogging
       case 'b':
