84e0eec31adf4147efd0c0141340ecabd85d29d3
G.Cai <ic.ve.cv@gmail.com>
2017-03-05 07:39:39 +1100

    implemented buffer/diff print change current_hostscript.py to unix line ending

---
+++ hostscripts/lib/__init__.py
@@ -0,0 +1,1 @@
+from .lcdControl import *
--- hostscripts/lib/lcdControl.py
+++ hostscripts/lib/lcdControl.py
@@ -4,12 +4,14 @@ from time import sleep
 
 class Lcd:
 
-    def __init__(self, dev, cmdmap="", size=(16, 2), bufsize=40, curpos=0, scpos=0, lcdParsecmd='x'):
+    def __init__(self, dev, cmdmap="", size=(16, 2), bufsize=40, curpos=0, scpos=0, lcdParsecmd='x', printOverHead=8):
         self.dev = dev
         self.size = size
         self.lcdParsecmd = lcdParsecmd
+        # printOverHead are used to determine whether to split print cmd when there is gap between txt cluster
+        self.printOverHead = printOverHead
         # create buffer
-        self.buf = [[" " for c in range(size[0])] for r in range(size[1])]
+        self.buf = [[" " for c in range(bufsize)] for r in range(size[1])]
         self.bufsize = bufsize
         self.curpos = curpos
         self.scpos = scpos
@@ -64,12 +66,82 @@ class Lcd:
         self.dev.write(("x" + chr(32) + chr(c + 1) + chr(r + 1) + ";").encode())
         self.updcur([c, r], True)
 
-    def print(self, txt):
+    def rprint(self, txt):
+        '''Directly prints text to display without diff'''
         self.dev.write((self.lcdParsecmd + chr(self.cmdmap["print"]) + txt + ";").encode())
         for i, char in enumerate(txt):
             c, r = self.convpos(i + self.curpos)
             self.buf[r][c] = char
         self.updcur(len(txt))
+
+    def getBuf(self, dig):
+        '''get char in buf at position (number not coordinate)'''
+        c, r = self.convpos(dig)
+        return self.buf[r][c]
+
+    def dPrint(self, txt):
+        '''diff/delta print'''
+        txtCluster = []
+        clusterPos = []
+        i = 0
+        while i < len(txt):
+            c, r = self.convpos(i + self.curpos)
+            # new cluster
+            if self.buf[r][c] != txt[i]:
+                clusterPos.append([c, r])
+                # for storng diff char
+                temptxt = []
+                temptxt.append(txt[i])
+                i += 1
+                gap = 0
+
+                # examine cluster and count gap
+                while gap < self.printOverHead and i < len(txt):
+                    c, r = self.convpos(i + self.curpos)
+                    if self.buf[r][c] != txt[i]:
+                        temptxt.append(txt[i])
+                        i += 1
+                    else:
+                        gap = 0
+                        gaptxt = []
+                        # discard flag
+                        discard = False
+                        while not discard:
+                            if (not i < len(txt)) or gap >= self.printOverHead:
+                                discard = True
+                                break
+                            elif self.getBuf(i + self.curpos) != txt[i]:
+                                break
+                            else:
+                                gaptxt.append(txt[i])
+                            i += 1
+                            gap += 1
+                        if not discard:
+                            temptxt = temptxt + gaptxt
+
+                txtCluster.append("".join(temptxt))
+            # continue to find cluster
+            else:
+                i += 1
+        return clusterPos, txtCluster
+
+    def print(self, txt):
+        '''method that calls the dprint and rprint'''
+        cood, txts = self.dPrint(txt)
+
+        if len(cood) != len(txts):
+            raise IndexError("cood and txts does not match")
+        if len(cood) == 0:
+            # print("no-op")
+            return
+        for c, t in zip(cood, txts):
+            rw, cl = c
+            if not self.convpos([rw, cl]) == self.curpos:
+                self.setCursor(rw, cl)
+                # print("s: {}".format([rw, cl]))
+
+            self.rprint(t)
+            # print("p: {}".format(t))
 
     def updcur(self, val, raw=False):
         'flag raw to set instead of displace curpos'
