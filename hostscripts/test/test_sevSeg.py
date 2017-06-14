d2ffd18ed20ca05643bb7d8021fd5fd9834ab915
G.Cai <ic.ve.cv@gmail.com>
2017-06-14 20:08:06 +1000

    updated test to use pop

--- hostscripts/test/test_sevSeg.py
+++ hostscripts/test/test_sevSeg.py
@@ -11,6 +11,11 @@ class tsDev:
     def clr(self):
         self.opt = None
 
+    def pop(self):
+        rlt = self.opt
+        self.clr()
+        return rlt
+
 def test_dev_id_wrapper(func):
 
     def wrp_func(self):
@@ -31,48 +36,46 @@ class TestSevSeg(unittest.TestCase):
     @test_dev_id_wrapper
     def test_clear(self, dev_id):
         self.ss.clear()
-        self.assertEqual(self.dev.opt, "0{};".format(dev_id).encode())
+        self.assertEqual(self.dev.pop(), "0{};".format(dev_id).encode())
 
     def test_clear_devid(self):
         self.ss.dev_id = 1
         self.ss.clear()
-        self.assertEqual(self.dev.opt, b"01;")
+        self.assertEqual(self.dev.pop(), b"01;")
 
     def test_printrow(self):
         self.ss.printrow("FF")
-        self.assertEqual(self.dev.opt, b"r0FF;")
+        self.assertEqual(self.dev.pop(), b"r0FF;")
 
     def test_printcol(self):
         self.ss.printcol("FF")
-        self.assertEqual(self.dev.opt, b"c0FF;")
+        self.assertEqual(self.dev.pop(), b"c0FF;")
 
     def test_setled(self):
         self.ss.setled(1,2,1)
-        self.assertEqual(self.dev.opt, b"l0121;")
+        self.assertEqual(self.dev.pop(), b"l0121;")
 
     def test_setstate(self):
         self.ss.setstate(0)
-        self.assertEqual(self.dev.opt, b"s00;")
+        self.assertEqual(self.dev.pop(), b"s00;")
 
     def test_setintensity(self):
         self.ss.setintensity(15)
-        self.assertEqual(self.dev.opt, b"i015;")
+        self.assertEqual(self.dev.pop(), b"i015;")
 
     def test_write_one_char(self):
         self.ss.write("-", 1, 1)
-        self.assertEqual(self.dev.opt, b"a01-1;")
+        self.assertEqual(self.dev.pop(), b"a01-1;")
 
     def test_onstate_buffer(self):
         self.ss.setstate(0)
-        self.assertEqual(self.dev.opt, b"s00;")
-        self.dev.clr()
+        self.assertEqual(self.dev.pop(), b"s00;")
         self.ss.setstate(0)
-        self.assertIsNone(self.dev.opt)
+        self.assertIsNone(self.dev.pop())
 
         self.ss.setstate(1)
-        self.assertEqual(self.dev.opt, b"s01;")
-        self.dev.clr()
+        self.assertEqual(self.dev.pop(), b"s01;")
         self.ss.setstate(1)
-        self.assertIsNone(self.dev.opt)
+        self.assertIsNone(self.dev.pop())
 
 
